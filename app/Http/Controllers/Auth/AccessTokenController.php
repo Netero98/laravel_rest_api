4799f7231bfe8a548ee9afd5dcbb9367e57b709e
Daniil Stepanov <Neoanalitik@gmail.com>
2023-12-05 18:06:25 +0700

    Added access token routes + moved auth routes to api dir

---
+++ app/Http/Controllers/Auth/AccessTokenController.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
+
+final class AccessTokenController extends Controller
+{
+    private const PARAM_TOKEN_NAME = 'token_name';
+
+    public function create(LoginRequest $request)
+    {
+        $request->authenticate();
+
+        $validator = Validator::make($request->input(), [
+            self::PARAM_TOKEN_NAME => 'required',
+        ]);
+
+        $controller = $this;
+
+        $validator->after(static function($validator) use ($request, $controller) {
+            $token = $controller->findTokenByName($request->user(), $request->{self::PARAM_TOKEN_NAME});
+
+            if ($token instanceof PersonalAccessToken) {
+                $validator->errors()->add(
+                    self::PARAM_TOKEN_NAME, "Token with name '{$request->{self::PARAM_TOKEN_NAME}}' already exists"
+                );
+            }
+        });
+        $validator->validate();
+
+        $token = $request->user()->createToken($request->{self::PARAM_TOKEN_NAME});
+
+        return $token->plainTextToken;
+    }
+
+    public function revokeAll(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->noContent();
+    }
+
+    public function revokeCurrent(Request $request): Response
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+
+    private function findTokenByName(User $user, string $name): ?PersonalAccessToken
+    {
+        $token = $user->tokens()->where('name', $name)->first();
+
+        return $token instanceof PersonalAccessToken ? $token : null;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AccessTokenController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['guest'])->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->name('register');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware('guest')
+        ->name('login');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.email');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.store');
+
+    Route::post('/token', [AccessTokenController::class, 'create'])
+        ->name('create-token');
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['throttle:6,1'])
+        ->name('verification.send');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+    Route::post('/token/revoke-all', [AccessTokenController::class, 'revokeAll'])->name('revoke-all');
+
+    Route::post('/token/revoke-current', [AccessTokenController::class, 'revokeCurrent'])->name('revoke-current');
 });
